7089340ff24fee6055741d2d4c8d27d7dd239f6a
jdeeny <jdeeny@gmail.com>
2019-01-19 22:11:32 -0700

    barest bits of a library

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
 package.path = "../?.lua;" .. package.path
 local midi = require 'luamidi'
 
+local class = require 'lib.middleclass.middleclass'
+
 local Treble = {}
+
+function Treble.update()
+
+end
+
+function Treble.isNote()
+end
 
 return Treble
